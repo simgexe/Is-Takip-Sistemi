8d18a5dd1cd708069e5f33d1f95005af54826ff0
simge <egmis2001@gmail.com>
2025-01-15 17:42:21 +0300

    isleri listeleme, takip

--- IsTakipSistemi/Controllers/YoneticiController.cs
+++ IsTakipSistemi/Controllers/YoneticiController.cs
@@ -38,6 +38,7 @@ namespace IsTakipSistemi.Controllers
 
                 ViewBag.personeller = calisanlar;
                 var birim = (from b in entity.Birimler where b.birimId == birimId select b).FirstOrDefault();
+                
                 ViewBag.birimAd = birim.birimAd; 
                 return View();
             }
@@ -51,8 +52,10 @@ namespace IsTakipSistemi.Controllers
         {
             string isBaslik = formCollection["isBaslik"];
             string isAciklama = formCollection["isAciklama"];
-            int secilenPersonelId =Convert.ToInt32( formCollection["selectPer"]);
+            int secilenPersonelId =Convert.ToInt32( formCollection["selectPer"]); 
+            
             Isler yeniIs = new Isler();
+             
             yeniIs.isBaslik = isBaslik;
             yeniIs.isAciklama = isAciklama;
             yeniIs.isPersonelId = secilenPersonelId;
@@ -75,13 +78,57 @@ namespace IsTakipSistemi.Controllers
 
                 ViewBag.personeller = calisanlar;
                 var birim = (from b in entity.Birimler where b.birimId == birimId select b).FirstOrDefault();
+               
                 ViewBag.birimAd = birim.birimAd;
+
                 return View();
             }
             else
             {
                 return RedirectToAction("Index", "Login");
             }
+        }
+        [HttpPost]
+        public ActionResult Takip(int selectPer)
+        {
+            var secilenPersonel = (from p in entity.Personeller where p.personelId == selectPer select p).FirstOrDefault();
+
+            TempData["secilen"] = secilenPersonel;
+            return RedirectToAction("Listele", "Yonetici");
+        }
+        [HttpGet] 
+        public ActionResult Listele()
+        {
+            int yetkiTurId = Convert.ToInt32(Session["PersonelYetkiTurId"]);
+
+            if(yetkiTurId == 1)
+            { 
+                Personeller secilenPersonel = (Personeller)TempData["secilen"];
+
+                try
+                {
+
+                    //secilen personelin idsine göre işlerini listeleme
+                    var isler = (from i in entity.Isler 
+                                 where i.isPersonelId == secilenPersonel.personelId 
+                                 select i).ToList().OrderByDescending(i => i.iletilenTarih);
+                   
+                    ViewBag.isler = isler;
+                    ViewBag.personel = secilenPersonel;
+                    return View();
+                }
+                catch(Exception)
+                {
+                    return RedirectToAction("Takip", "Yonetici");
+                }
+
+               
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            
         }
     }
 }
