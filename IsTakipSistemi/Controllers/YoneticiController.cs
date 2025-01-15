39586cf3961f6ebb258ed94f8962a66f0a0b40d9
simge <egmis2001@gmail.com>
2025-01-15 16:19:09 +0300

    login ve iş atama

---
+++ IsTakipSistemi/Controllers/YoneticiController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IsTakipSistemi.Models;
+
+namespace IsTakipSistemi.Controllers
+{
+    public class YoneticiController : Controller
+    {
+        IsTakipDBEntities entity = new IsTakipDBEntities();
+        // GET: Yonetici
+        public ActionResult Index()
+        {   //yetki olmadan başka indexlere geçilmesin diye
+            int yetkiTurId = Convert.ToInt32(Session["PersonelYetkiTurId"]);
+
+            if(yetkiTurId == 1)
+            {
+                int birimId = Convert.ToInt32(Session["PersonelBirimId"]);
+                var birim = (from b in entity.Birimler where b.birimId == birimId select b).FirstOrDefault();
+                ViewBag.birimAd = birim.birimAd;
+
+                return View();
+            }
+            else { 
+            return RedirectToAction("Index","Login");
+            }
+        }
+        public ActionResult Ata()
+        {
+            int yetkiTurId = Convert.ToInt32(Session["PersonelYetkiTurId"]);
+
+            if (yetkiTurId == 1)
+            {
+                int birimId = Convert.ToInt32(Session["PersonelBirimId"]);
+                var calisanlar = (from p in entity.Personeller where p.personelBirimId == birimId && p.personelYetkiTurId == 2 select p).ToList();
+
+                ViewBag.personeller = calisanlar;
+                var birim = (from b in entity.Birimler where b.birimId == birimId select b).FirstOrDefault();
+                ViewBag.birimAd = birim.birimAd; 
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+        [HttpPost]
+        public ActionResult Ata(FormCollection formCollection )
+        {
+            string isBaslik = formCollection["isBaslik"];
+            string isAciklama = formCollection["isAciklama"];
+            int secilenPersonelId =Convert.ToInt32( formCollection["selectPer"]);
+            Isler yeniIs = new Isler();
+            yeniIs.isBaslik = isBaslik;
+            yeniIs.isAciklama = isAciklama;
+            yeniIs.isPersonelId = secilenPersonelId;
+            yeniIs.iletilenTarih = DateTime.Now;
+            yeniIs.isDurumId = 1;
+
+            entity.Isler.Add(yeniIs);
+            entity.SaveChanges();
+            
+            return RedirectToAction("Takip","Yönetici");
+        }
+        public ActionResult Takip()
+        {
+            int yetkiTurId = Convert.ToInt32(Session["PersonelYetkiTurId"]);
+
+            if (yetkiTurId == 1)
+            {
+                int birimId = Convert.ToInt32(Session["PersonelBirimId"]);
+                var calisanlar = (from p in entity.Personeller where p.personelBirimId == birimId && p.personelYetkiTurId == 2 select p).ToList();
+
+                ViewBag.personeller = calisanlar;
+                var birim = (from b in entity.Birimler where b.birimId == birimId select b).FirstOrDefault();
+                ViewBag.birimAd = birim.birimAd;
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+    }
+}
